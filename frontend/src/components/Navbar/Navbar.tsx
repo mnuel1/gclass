6ba23243cf420c1b267b68c79534f1a24f560f6b
mnuel1 <mnuelmrin21@gmail.com>
2024-12-01 20:51:07 +0800

    update

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
-import logo from "../../assets/5.png";
+import logo from "../../../public/logo.png";
 type MainMenuProps = {
   children: React.ReactNode;
 };
